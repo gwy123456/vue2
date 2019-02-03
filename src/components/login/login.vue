1fd00d1301fb90a95084f7e15c2ffbb71df6c2fe
guowenyao <286696460@qq.com>
2019-02-03 21:48:25 +0800

    Update login.vue
    login静态

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -3,22 +3,23 @@
     <!-- login.vue -->
     <div class="content">
         <!-- 表单组件 -->
+        <h2>用户登录</h2>
       <el-form
-        :model="ruleForm2"
+        label-position="top"
+        :model="loginObj"
         status-icon
         ref="ruleForm2"
         label-width="100px"
         class="demo-ruleForm"
       >
         <el-form-item label="用户名">
-          <el-input type="text" v-model="ruleForm2.pass" autocomplete="off"></el-input>
+          <el-input type="text" v-model="loginObj.username" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item label="密码" prop="checkPass">
-          <el-input type="password" v-model="ruleForm2.checkPass" autocomplete="off"></el-input>
+          <el-input type="password" v-model="loginObj.password" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
-          <el-button @click="resetForm('ruleForm2')">重置</el-button>
+          <el-button class="mybtn" type="primary" @click="submitForm('ruleForm2')">登陆</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -30,7 +31,8 @@ export default {
     data() {
         return {
             loginObj: {
-                
+                username:'',
+                password:''
             }
         }
     }
@@ -53,5 +55,11 @@ export default {
   left: 50%;
   transform: translate(-50%, -50%);
   border-radius: 5px;
+}
+h2 {
+  margin-bottom: 15px;
+}
+.mybtn {
+  width: 100%;
 }
 </style>
